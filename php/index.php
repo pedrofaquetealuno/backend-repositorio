cfcc3fb7a309979dc94a17c83afaa569f47187af
pedrofaquetealuno <pedro.faquete.senai404@gmail.com>
2024-08-02 10:44:11 -0300

    variaveis

---
+++ php/index.php
@@ -0,0 +1,23 @@
+<?php
+$nome = 'Pedro';
+$escola = 'SESI';
+$cidade = 'Cerquilho';
+$CE = 428 ;
+$curso = 'Desenvolvimento de Sistemas';
+$media = 9.5;
+
+// as variaveis estão a baixo :)
+echo 'Olá, bem vindo ao SESI-SP <br>';
+
+echo "$nome <br>";
+echo "$escola <br>";
+echo "$cidade <br>";
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>";
+
+echo "Olá $nome, bem vindo ao SESI $CE de $cidade, sua nota no curso de $curso é: $media"
+
+
+
+?>
