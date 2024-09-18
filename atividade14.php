746c385f0bd13a86cb0f4df02e6543f9dbd7268b
pedrofaquetealuno <pedro.faquete.senai404@gmail.com>
2024-09-18 16:21:06 -0300

    Add files via upload

---
+++ atividade14.php
@@ -0,0 +1,34 @@
+<?php
+// Cria um array multi
+$usuario = [
+  [ "Nome" => "Pedro Faquete",
+  "cidade" => "Cerquilho",
+  "Estado" => "SP"],
+  [ "Nome" => "Daniel Pão",
+  "cidade" => "Cascavel",
+  "Estado" => "PR"],
+  [ "Nome" => "Pedro Miguel",
+  "cidade" => "Balneario",
+  "Estado" => "SC"],
+  [ "Nome" => "Ana Vitoria",
+  "cidade" => "Copa Cabana",
+  "Estado" => "RJ"],
+  [ "Nome" => "Nathan Fernandes",
+  "cidade" => "Cana verde",
+  "Estado" => "MG"],
+];
+
+//Define variavel
+
+$busca = "Pedro Faquete";
+
+//estrutura
+echo '<pre>';
+print_r($usuario);
+echo '</pre>';
+?>
+
+
+
+
+
