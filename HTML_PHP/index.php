26de15f3ad4f77b6911a16acd5fce71d847f52b0
pedrofaquetealuno <pedro.faquete.senai404@gmail.com>
2024-10-02 14:00:11 -0300

    Add files via upload

---
+++ HTML_PHP/index.php
@@ -0,0 +1,23 @@
+<?php
+$nome='Valter';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bem-vindo
+        <?php
+        echo $nome;
+        ?>
+        <br>
+        <?php
+        echo 'Olá meu SENAI';
+        ?>
+    </h1>
+</body>
+</html>
---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<pre>";
+print_r ($_POST);
+echo "</pre>";
+echo "<hr>";
+
+echo "E-mail: ";
+echo $_POST['email'];
+echo "<br>";
+echo $_POST['senha'];
+
+
+?>
